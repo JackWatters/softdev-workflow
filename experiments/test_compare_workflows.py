5b7350a29dc886c552cbc471ee5e8efc583d6ab7
twsswt <timothy.storer@glasgow.ac.uk>
2016-04-17 22:55:29 +0100

    Removed munged builder pattern from software project.

--- experiments/test_compare_workflows.py
+++ experiments/test_compare_workflows.py
@@ -1,15 +1,16 @@
 import unittest
 
-from softdev_model.system.software.bug import Bug
-from softdev_model.system.software.chunk import Chunk
-from softdev_model.system.software.feature import Feature
-from softdev_model.system.software.software_project import SoftwareProject
-from softdev_model.system.software.test import Test
+import fuzzi_moss
+
+from random import Random
+
+from softdev_model.system import Bug, Chunk, Developer, Feature, SoftwareProject, SoftwareSystem, Test
 from softdev_model.workflows.test_driven_development import TestDrivenDevelopment
 from softdev_model.workflows.waterfall import Waterfall
 
 
 class TestCompareWorkFlows(unittest.TestCase):
+    fuzzi_moss.fuzz.enable_fuzzings = False
 
     def setUp(self):
         Chunk._count = 0
@@ -17,24 +18,47 @@ class TestCompareWorkFlows(unittest.TestCase):
         Test._count = 0
         Bug._count = 0
 
-        self.waterfall_projects =\
-            [SoftwareProject.create_software_project(i, Waterfall,             250, [3, 5, 7], 50) for i in range(0, 10)]
-        self.tdd_projects =\
-            [SoftwareProject.create_software_project(i, TestDrivenDevelopment, 250, [3, 5, 7], 50) for i in range(0, 10)]
+        def create_software_projects(workflow):
+            return [SoftwareProject(
+                random=Random(seed),
+                software_system=SoftwareSystem(),
+                workflow=workflow,
+                developer=Developer(person_time=250),
+                schedule=[3, 5, 7],
+                number_of_traces=50,
+                max_trace_length=750) for seed in range(0, 10)]
 
-    @staticmethod
-    def build_software_systems(projects):
-        for project in projects:
-            project.build_and_operate()
-
-        print reduce(lambda x, y: x + y, map(lambda p: p.software_system.mean_operations_to_failure, projects), 0)
-        print reduce(lambda x, y: x + y, map(lambda p: p.developer.person_time, projects), 0)
+        self.waterfall_projects = create_software_projects(Waterfall)
+        self.tdd_projects = create_software_projects(TestDrivenDevelopment)
 
     def test_compare_with_excess_resource(self):
-        print "Waterfall"
-        self.build_software_systems(self.waterfall_projects)
-        print "TDD"
-        self.build_software_systems(self.tdd_projects)
+        for project in self.waterfall_projects + self.tdd_projects:
+            project.build_and_operate()
+
+        print "Work flow,\tMTF,\tRDT"
+        print TestCompareWorkFlows.create_result_row("Waterfall", self.waterfall_projects)
+        print TestCompareWorkFlows.create_result_row("TDD", self.tdd_projects)
+
+    @staticmethod
+    def average_project_attribute(projects, attr):
+        return reduce(lambda x, y: x + y, map(attr, projects), 0) / len(projects)
+
+    @staticmethod
+    def average_project_mean_time_to_failure(projects):
+        return TestCompareWorkFlows. \
+            average_project_attribute(projects, lambda p: p.software_system.mean_operations_to_failure)
+
+    @staticmethod
+    def average_project_remaining_developer_time(projects):
+        return TestCompareWorkFlows. \
+            average_project_attribute(projects, lambda p: p.developer.person_time)
+
+    @staticmethod
+    def create_result_row(workflow_label, projects):
+        return ",\t".join([workflow_label,
+                           str(TestCompareWorkFlows.average_project_mean_time_to_failure(projects)),
+                           str(TestCompareWorkFlows.average_project_remaining_developer_time(projects))])
+
 
 if __name__ == '__main__':
     unittest.main()
--- softdev_model/system/software_project.py
+++ softdev_model/system/software_project.py
@@ -4,41 +4,30 @@
 """
 
 from .bug import BugEncounteredException
-from .developer import Developer, DeveloperExhaustedException
+from .developer import DeveloperExhaustedException
 from .feature import InoperableFeatureException
-from .software_system import SoftwareSystem
-
-from random import Random
 
 
 class SoftwareProject(object):
     """
     Represents the overall state and behaviour of a software project.
     """
-    def __init__(self, random, software_system, workflow, developer, schedule, operations):
+    def __init__(self, random, software_system, workflow, developer, schedule, number_of_traces, max_trace_length):
         self.random = random
         self.software_system = software_system
         self.workflow = workflow
         self.developer = developer
         self.schedule = schedule
-        self.operations = operations
+        self.number_of_traces = number_of_traces
+        self.max_trace_length = max_trace_length
 
     def build_and_operate(self):
         try:
             self.workflow.work(self.random, self.developer, self.schedule)
         except DeveloperExhaustedException:
             pass
-        for _ in range(0, self.operations):
+        for _ in range(0, self.number_of_traces):
             try:
-                self.software_system.operate(self.random, 750)
+                self.software_system.operate(self.random, self.max_trace_length)
             except BugEncounteredException or InoperableFeatureException:
                 pass
-
-    @staticmethod
-    def create_software_project(seed, workflow_constructor, resources, schedule, operations):
-        developer = Developer(resources)
-        software_system = SoftwareSystem()
-        workflow = workflow_constructor(software_system)
-        random = Random(seed)
-        return SoftwareProject(random, software_system, workflow, developer, schedule, operations)
-
