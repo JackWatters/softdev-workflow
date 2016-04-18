205ba03d6c701f313a7fdcb85a4e30073594c89b
twsswt <timothy.storer@glasgow.ac.uk>
2016-04-18 12:42:54 +0100

    Tidied up setup.
     * Cleaned up the project description (broken by a bad refactoring).
     * Removed platform specific path separator

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name='Software_Dev',
     version='0.1',
     packages=['softdev_model'],
-    package_dir={'': './' },
+    package_dir={'': '.'},
     url='https://github.com/twsswt/software-dev',
     license='',
     author='Tim Storer',
     author_email='timothy.storer@glasgow.ac.uk',
-    description='A socio-technical softdev_model of software development work flows developed for the Fuzzi_Moss framework.',
-    setup_requires=['fuzzi_moss','nose'],
+    description='A socio-technical model of software development work flows developed for the Fuzzi_Moss '
+                'framework.',
+    setup_requires=['fuzzi_moss', 'nose'],
     test_suite='nose.collector',
     tests_require=['mock', 'nose']
 )
