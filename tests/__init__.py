526988e7156773c3085fb6d833edf2f6695e50c8
twsswt <timothy.storer@glasgow.ac.uk>
2016-07-08 14:34:18 +0100

    Re-introduced imports (some sort of path problem in different nose versions or python platforms).

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+from test_chunk import ChunkTest
+from test_developer import DeveloperTestCase
+from test_feature import FeatureTest
+from test_softwaresystem import SoftwareSystemTest
+from test_test import TestTest
+from test_test_driven_development import TestDrivenDevelopmentTest
+from test_waterfall import WaterfallTest
