b1c46ae03f7ad27287b5cb79e4cb5db3e87238f8
twsswt <timothy.storer@glasgow.ac.uk>
2016-11-09 21:42:37 +0000

    Fixed broken import in package module file.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,6 @@ from test_software_project import SoftwareProjectTestCase
 from test_softwaresystem import SoftwareSystemTest
 from test_test import TestTest
 from test_test_driven_development import TestDrivenDevelopmentTestCase
-from test_team import TDDDevelopmentTeamTestCase
 
 #from regression_test_tdd_developer import TDDDeveloperRegressionTestCase
 #from regression_test_waterfall import WaterfallRegressionTestCase
