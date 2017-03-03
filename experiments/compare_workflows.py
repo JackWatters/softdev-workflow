68173ffbf09a2ab0b06ac4b78bb81a27c99ba791
twsswt <timothy.storer@glasgow.ac.uk>
2017-03-03 09:57:13 +0000

    Refactored compare workflows to remove unnecessary encapsulation and reduce LoC.

--- experiments/compare_workflows.py
+++ experiments/compare_workflows.py
@@ -14,109 +14,6 @@ from softdev_model.workflows import ChangeManagement, Debugging, Implementation,
 
 headers_printed = False
 
-
-def print_result_row(result_specification):
-    global headers_printed
-    if not headers_printed:
-        headers = map(lambda t: t[0], result_specification)
-        print ", ".join(map(lambda h: h.rjust(10, ' '), headers))
-        headers_printed = True
-
-    row_format_string = ", ".join(map(lambda t: t[1], result_specification))
-    data = map(lambda t: t[2], result_specification)
-
-    print row_format_string % tuple(data)
-
-
-def run_simulation(fuzz_classes, plan, team_size, max_clock_tick, concentration):
-
-    random = SystemRandom(1)
-    pydysofu.pydysofu_random.seed(1)
-
-    workflow_advice = {
-        ChangeManagement.commit_changes:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
-        Specification.add_feature:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
-        Implementation.implement_feature:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
-        Testing.test_per_chunk_ratio:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
-        Debugging.debug_feature:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
-        Refactoring.refactor_feature:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
-        TestDrivenDevelopment.implement_feature_tdd:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
-        Waterfall.allocate_tasks:
-            incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration))
-    }
-
-    for clazz in fuzz_classes:
-        pydysofu.fuzz_clazz(clazz, workflow_advice)
-
-    projects_group = SoftwareProjectGroup(specification, plan, team_size, max_clock_tick,
-                                          number_of_projects=20,
-                                          number_of_traces=10,
-                                          max_trace_length=750,
-                                          random=random)
-
-    projects_group.build_and_operate()
-
-    result_specification = (
-        # Preamble
-        ("workflow",    "%-10s", plan.__name__[0:10]),
-        ("t_alloc",     "%10d",  max_clock_tick),
-        ("#team",       "%10d",  team_size),
-
-        # Fuzz parameters
-        ("fuzzed",     "%-10s",  reduce(lambda a, b: a+b, map(lambda c: c.__name__[0], fuzz_classes), "")),
-        ("co",         "%10.3f", concentration),
-
-        # Task counts
-        ("#exec_wf",   "%10d",   projects_group.task_count(Waterfall.allocate_tasks)),
-        ("#exec_tdd",  "%10d",   projects_group.task_count(TestDrivenDevelopment.implement_feature_tdd)),
-        ("#exec_co",   "%10d",   projects_group.task_count(ChangeManagement.commit_changes)),
-        ("#exec_spc",  "%10d",   projects_group.task_count(Specification.add_feature)),
-        ("#exec_imp",  "%10d",   projects_group.task_count(Implementation.add_chunk)),
-        ("#exec_tng",  "%10d",   projects_group.task_count(Testing.test_per_chunk_ratio)),
-        ("#exec_dbg",  "%10d",   projects_group.task_count(Debugging.debug_feature)),
-        ("#exec_rfg",  "%10d",   projects_group.task_count(Refactoring.refactor_feature)),
-
-        # Fuzz counts
-        ("#fuzz_wf",   "%10d",   fuzzi_moss.lines_removed_count(Waterfall)),
-        ("#fuzz_tdd",  "%10d",   fuzzi_moss.lines_removed_count(TestDrivenDevelopment)),
-        ("#fuzz_co",   "%10d",   fuzzi_moss.lines_removed_count(ChangeManagement)),
-        ("#fuzz_spc",  "%10d",   fuzzi_moss.lines_removed_count(Specification)),
-        ("#fuzz_imp",  "%10d",   fuzzi_moss.lines_removed_count(Implementation)),
-        ("#fuzz_tng",  "%10d",   fuzzi_moss.lines_removed_count(Testing)),
-        ("#fuzz_dbg",  "%10d",   fuzzi_moss.lines_removed_count(Debugging)),
-        ("#fuzz_rfg",  "%10d",   fuzzi_moss.lines_removed_count(Refactoring)),
-        ("#fuzz_tot",  "%10d",   fuzzi_moss.lines_removed_count()),
-
-        ("#lines_wf",  "%10d",   fuzzi_moss.removable_lines_count(Waterfall.allocate_tasks)),
-        ("#lines_tdd", "%10d",   fuzzi_moss.removable_lines_count(TestDrivenDevelopment.implement_feature_tdd)),
-        ("#lines_co",  "%10d",   fuzzi_moss.removable_lines_count(ChangeManagement.commit_changes)),
-        ("#lines_spc", "%10d",   fuzzi_moss.removable_lines_count(Specification.add_feature)),
-        ("#lines_imp", "%10d",   fuzzi_moss.removable_lines_count(Implementation.add_chunk)),
-        ("#lines_tng", "%10d",   fuzzi_moss.removable_lines_count(Testing.test_per_chunk_ratio)),
-        ("#lines_dbg", "%10d",   fuzzi_moss.removable_lines_count(Debugging.debug_feature)),
-        ("#lines_rfg", "%10d",   fuzzi_moss.removable_lines_count(Refactoring.refactor_feature)),
-
-        # Project characteristics
-        ("mtf", "%10.2f", projects_group.average_project_mean_time_to_failure),
-        ("t_used", "%10d", projects_group.average_project_time_used),
-        ("#ftrs", "%10.2f", projects_group.average_project_features_implemented),
-        ("#t_run", "%10d", projects_group.simulation_duration)
-    )
-
-    print_result_row(result_specification)
-
-    pydysofu.reset_invocation_counters()
-    fuzzi_moss.reset_lines_removed_counters()
-    pydysofu.defuzz_all_classes()
-
-
 specification = [UserStory(0, 3, 1), UserStory(1, 5, 2), UserStory(2, 7, 3)]
 
 experimental_parameters = [
@@ -133,8 +30,102 @@ experimental_parameters = [
         for concentration in [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1.0, 5.0]
     ]
 
-for experimental_parameter in experimental_parameters:
+for fuzz_classes, plan, team_size, max_clock_tick, concentration in experimental_parameters:
     try:
-        run_simulation(*experimental_parameter)
+        random = SystemRandom(1)
+        pydysofu.pydysofu_random.seed(1)
+
+        workflow_advice = {
+            ChangeManagement.commit_changes:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
+            Specification.add_feature:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
+            Implementation.implement_feature:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
+            Testing.test_per_chunk_ratio:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
+            Debugging.debug_feature:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
+            Refactoring.refactor_feature:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
+            TestDrivenDevelopment.implement_feature_tdd:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration)),
+            Waterfall.allocate_tasks:
+                incomplete_procedure(random, pmf=default_incomplete_procedure_pmf(concentration))
+        }
+
+        for clazz in fuzz_classes:
+            pydysofu.fuzz_clazz(clazz, workflow_advice)
+
+        projects_group = SoftwareProjectGroup(specification, plan, team_size, max_clock_tick,
+                                              number_of_projects=20,
+                                              number_of_traces=10,
+                                              max_trace_length=750,
+                                              random=random)
+
+        projects_group.build_and_operate()
+
+        result_specification = (
+            # Preamble
+            ("workflow", "%-10s", plan.__name__[0:10]),
+            ("t_alloc", "%10d", max_clock_tick),
+            ("#team", "%10d", team_size),
+
+            # Fuzz parameters
+            ("fuzzed", "%-10s", reduce(lambda a, b: a + b, map(lambda c: c.__name__[0], fuzz_classes), "")),
+            ("co", "%10.3f", concentration),
+
+            # Task counts
+            ("#exec_wf", "%10d", projects_group.task_count(Waterfall.allocate_tasks)),
+            ("#exec_tdd", "%10d", projects_group.task_count(TestDrivenDevelopment.implement_feature_tdd)),
+            ("#exec_co", "%10d", projects_group.task_count(ChangeManagement.commit_changes)),
+            ("#exec_spc", "%10d", projects_group.task_count(Specification.add_feature)),
+            ("#exec_imp", "%10d", projects_group.task_count(Implementation.add_chunk)),
+            ("#exec_tng", "%10d", projects_group.task_count(Testing.test_per_chunk_ratio)),
+            ("#exec_dbg", "%10d", projects_group.task_count(Debugging.debug_feature)),
+            ("#exec_rfg", "%10d", projects_group.task_count(Refactoring.refactor_feature)),
+
+            # Fuzz counts
+            ("#fuzz_wf", "%10d", fuzzi_moss.lines_removed_count(Waterfall)),
+            ("#fuzz_tdd", "%10d", fuzzi_moss.lines_removed_count(TestDrivenDevelopment)),
+            ("#fuzz_co", "%10d", fuzzi_moss.lines_removed_count(ChangeManagement)),
+            ("#fuzz_spc", "%10d", fuzzi_moss.lines_removed_count(Specification)),
+            ("#fuzz_imp", "%10d", fuzzi_moss.lines_removed_count(Implementation)),
+            ("#fuzz_tng", "%10d", fuzzi_moss.lines_removed_count(Testing)),
+            ("#fuzz_dbg", "%10d", fuzzi_moss.lines_removed_count(Debugging)),
+            ("#fuzz_rfg", "%10d", fuzzi_moss.lines_removed_count(Refactoring)),
+            ("#fuzz_tot", "%10d", fuzzi_moss.lines_removed_count()),
+
+            ("#lines_wf", "%10d", fuzzi_moss.removable_lines_count(Waterfall.allocate_tasks)),
+            ("#lines_tdd", "%10d", fuzzi_moss.removable_lines_count(TestDrivenDevelopment.implement_feature_tdd)),
+            ("#lines_co", "%10d", fuzzi_moss.removable_lines_count(ChangeManagement.commit_changes)),
+            ("#lines_spc", "%10d", fuzzi_moss.removable_lines_count(Specification.add_feature)),
+            ("#lines_imp", "%10d", fuzzi_moss.removable_lines_count(Implementation.add_chunk)),
+            ("#lines_tng", "%10d", fuzzi_moss.removable_lines_count(Testing.test_per_chunk_ratio)),
+            ("#lines_dbg", "%10d", fuzzi_moss.removable_lines_count(Debugging.debug_feature)),
+            ("#lines_rfg", "%10d", fuzzi_moss.removable_lines_count(Refactoring.refactor_feature)),
+
+            # Project characteristics
+            ("mtf", "%10.2f", projects_group.average_project_mean_time_to_failure),
+            ("t_used", "%10d", projects_group.average_project_time_used),
+            ("#ftrs", "%10.2f", projects_group.average_project_features_implemented),
+            ("#t_run", "%10d", projects_group.simulation_duration)
+        )
+
+        if not headers_printed:
+            headers = map(lambda t: t[0], result_specification)
+            print ", ".join(map(lambda h: h.rjust(10, ' '), headers))
+            headers_printed = True
+
+        row_format_string = ", ".join(map(lambda t: t[1], result_specification))
+        data = map(lambda t: t[2], result_specification)
+
+        print row_format_string % tuple(data)
+
+        pydysofu.reset_invocation_counters()
+        fuzzi_moss.reset_lines_removed_counters()
+        pydysofu.defuzz_all_classes()
+
     except Exception as e:
-        print >> sys.stderr, e, "encountered during run of", str(experimental_parameter), "."
+        print >> sys.stderr, e, "encountered during run of",
+        print str((fuzz_classes, plan, team_size, max_clock_tick, concentration)), "."
